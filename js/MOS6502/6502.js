8196ebe1360f1d35f4e8d9e87f9f91f55c7bfdea
Brian Milton <brian.milton@ef.com>
2014-01-21 12:50:09 +0800

    MOS6502: Going to use an object mapped to functions for the opcodes. This way I can pass the addressing state into a normal function to alter the result. It makes sense at the moment... We'll see if it makes sense later. :-S

--- js/MOS6502/6502.js
+++ js/MOS6502/6502.js
@@ -41,22 +41,31 @@ var MOS6502 = function() {
     this._NMI = false;
 
     /* Registers */
-    this._X = 0x00;
-    this._Y = 0x00;
-    this._P = { // Status
-        "N" : false,  // Negative
-        "V" : false,  // Overflow
-        "B" : false,  // Break
-        "D" : false,  // Decimal mode
-        "I" : false,  // IRQ disable
-        "Z" : false,  // Zero
-        "C" : false   // Carry
-    };
+    this._X = 0x00; // X Register
+    this._Y = 0x00; // Y Register
     this._A = 0x00; // Accumulator
 
+    /* Status Register */
+    /* This one requires a little explanation.
+     * The status register is a single byte. Each bit represents a state:
+     *
+     *   Bit: 7  6  5  4  3  2  1  0
+     * State: S  V     B  D  I  Z  C
+     *
+     *     C: Carry flag.               (Mask: 0x01)
+     *     Z: Zero flag.                (Mask: 0x02)
+     *     I: Interrupt enable/disable. (Mask: 0x04)
+     *     D: Decimal / BCD mode.       (Mask: 0x08)
+     *     B: Software interrupt (BRK)  (Mask: 0x10)
+     *     V: Overflow flag.            (Mask: 0x40)
+     *     S: Sign (Negative) Flag      (Mask: 0x80)
+     *
+     */
+    this._P = 0x00;
+
     /* Stack */
-    this._stack = new Uint8Array(0xFF);
-    this._sp = 0x00;
+    this._STACK = new Uint8Array(0xFF);
+    this._SP = 0x00;
 
     /* Program Counter */
     this._PC = 0x0000;  // This is usualy PCL and PCH, but we can combine them here into one.
@@ -72,3 +81,141 @@ var MOS6502 = function() {
     this._RAM = new Uint8Array(0xFFFF);  // 64k of RAM. NES uses more, but works around it.
 
 }
+
+/**
+ *
+ What follows is a list of all instructions. Depending on the addressing mode will dictate the OPCODE. Where there is
+ only one opcode, it will be listed along with the addressing mode.
+
+ ADC - Add memory to accumulator with carry.
+ AND - "AND" memory with accumulator.
+ ASL - Shift Left One Bit (Memory or Accumulator).
+ BCC - Branch on carry clear.
+ BCS - Branch on carry set.  [B0 - Relative Addressing]
+ BEQ - Branch on result zero.  [F0 - Relative Addressing]
+ BIT - Test bits in memory with accumulator.
+ BMI - Branch on result minus.  [30 - Relative Addressing]
+ BNE - Branch on result not zero.  [D0 - Relative Addressing]
+ BPL - Branch on result plus.  [10 - Relative Addressing]
+ BRK - Force break.  [00 - Implied]
+ BVC - Branch on overflow clear.  [50 - Relative Addressing]
+ BVS - Branch on overflow set.  [70 - Relative Addressing]
+ CLC - Clear carry flag.  [18 - Implied Addressing]
+ CLD - Clear decimal mode. [D8 - Implied Addressing]
+ CLI - Clear interrupt disable bit.  [58 - Implied Addressing]
+ CLV - Clear overflow flag.  [B8 - Implied Addressing]
+ CMP - Compare memory and accumulator.
+ CPX - Compare memory and index X.
+ CPY - Compare memory and index Y.
+ DEC - Decrement memory by one.
+ DEX - Decrement index X by one.  [CA - Implied Addressing]
+ DEY - Decrement index Y by one.  [88 - Implied Addressing]
+ EOR - "Exclusive-Or" memory with accumulator.
+ INC - Increment memory by one.
+ INX - Increment index X by one.  [E8 - Implied Addressing]
+ INY - Increment index Y by one.  [C8 - Implied Addressing]
+ JMP - Jump to new location.
+ JSR - Jump to new location, saving return address.  [20 - Absolute Addressing]
+ LDA - Load accumulator with memory.
+ LDX - Load index X with memory.
+ LDY - Load index Y with memory.
+ LSR - Shift right one bit (memory or accumulator).
+ NOP - No operation.  [EA - Implied Addressing]
+ ORA - "OR" memory with accumulator.
+ PHA - Push accumulator on stack.  [48 - Implied Addressing]
+ PHP - Push processor status on stack.  [08 - Implied Addressing]
+ PLA - Pull accumulator from stack.  [68 - Implied Addressing]
+ PLP - Pull processor status from stack.  [28 - Implied Addressing]
+ ROL - Rotate one bit left (memory or accumulator).
+ ROR - Rotate one bit right (memory or accumulator).
+ RTI - Return from interrupt.  [4D - Implied Addressing]
+ RTS - Return from subroutine.  [60 - Implied Addressing]
+ SBC - Subtract memory from accumulator with borrow.
+ SEC - Set carry flag.  [38 - Implied Addressing]
+ SED - Set decimal mode.  [F8 - Implied Addressing]
+ SEI - Set interrupt disable status.  [78 - Implied Addressing]
+ STA - Store accumulator in memory.
+ STX - Store index X in memory.
+ STY - Store index Y in memory.
+ TAX - Transfer accumulator to index X.  [AA - Implied Addressing]
+ TAY - Transfer accumulator to index Y.  [A8 - Implied Addressing]
+ TSX - Transfer stack pointer to index X.  [BA - Implied Addressing]
+ TXA - Transfer index X to accumulator.  [8A - Implied Addressing]
+ TXS - Transfer index X to stack pointer.  [9A - Implied Addressing]
+ TYA - Transfer index Y to accumulator.  [98 - Implied Addressing]
+
+ */
+
+var allTheOpcodes = {
+    // 0x0X
+    0x00 : "BRK",
+    0x01 : "ORA - (Indirect, X)",
+    0x02 : "",
+    0x03 : "",
+    0x04 : "",
+    0x05 : "ORA - Zero Page",
+    0x06 : "ASL - Zero Page",
+    0x07 : "",
+    0x08 : "PHP",
+    0x09 : "ORA - Immediate",
+    0x0A : "ASL - Accumulator",
+    0x0B : "",
+    0x0C : "",
+    0x0D : "ORA - Absolute",
+    0x0E : "ASL - Absolute",
+    0x0F : "",
+
+    // 0x1X
+    0x10 : "BPL",
+    0x11 : "ORA - (Indirect), Y",
+    0x12 : "",
+    0x13 : "",
+    0x14 : "",
+    0x15 : "ORA - Zero Page, X",
+    0x16 : "ASL - Zero Page, X",
+    0x17 : "",
+    0x18 : "CLC",
+    0x19 : "ORA - Absolute, Y",
+    0x1A : "",
+    0x1B : "",
+    0x1C : "",
+    0x1D : "ORA - Absolute, X",
+    0x1E : "ASL - Absolute, X",
+    0x1F : "",
+
+    // 0x2X
+    0x20 : "JSR",
+    0x21 : "AND - (Indirect, X)",
+    0x22 : "",
+    0x23 : "",
+    0x24 : "BIT - Zero Page",
+    0x25 : "AND - Zero Page",
+    0x26 : "ROL - Zero Page",
+    0x27 : "",
+    0x28 : "PLP",
+    0x29 : "AND - Immediate",
+    0x2A : "ROL - Accumulator",
+    0x2B : "",
+    0x2C : "BIT - Absolute",
+    0x2D : "AND - Absolute",
+    0x2E : "ROL - Accumulator",
+    0x2F : "",
+
+    // 0x3X
+    0x30 : "BMI",
+    0x31 : "AND - (Indirect), Y",
+    0x32 : "",
+    0x33 : "",
+    0x34 : "",
+    0x35 : "AND - Zero Page, X",
+    0x36 : "ROL - Zero Page, X",
+    0x37 : "",
+    0x38 : "SEC",
+    0x39 : "AND - Absolute, Y",
+    0x3A : "",
+    0x3B : "",
+    0x3C : "",
+    0x3D : "AND - Absolute, X",
+    0x3E : "ROL - Absolute, X",
+    0x3F : ""
+};
