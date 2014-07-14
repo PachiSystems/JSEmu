0ea9aecf58db19150feaf2b3d005a13208182d5f
Brian Milton <admin@javaproductions.net>
2014-07-14 15:06:19 +0100

    MOS6502: Issue #42. BEQ wasn't setting cycles correctly, but that shouldn't be causing the test ROM to fail... Well... One more left...

--- test/MOS6052/test.js
+++ test/MOS6052/test.js
@@ -18668,12 +18668,142 @@ test("0xEA - NOP (Implied)", function() {
 
 //</editor-fold>
 
+/*********************************************************************************************************************/
+
+//<editor-fold desc="BEQ Tests">
+
+QUnit.module("Instruction - BEQ", {
+    setup: function() {
+        MOS6502.init();
+    }
+});
+
+test("0xF0 - BEQ (Relative)", function() {
+    /**
+     *    Instruction = BEQ - Branch on result zero
+     * Affected Flags = None
+     *    Total Tests = 5
+     */
+
+    var OPCODE = 0xF0,
+        relativePlusSamePage = 64,
+        relativeMinusSamePage = 192,
+        relativePlusNextPage = 127,
+        relativeMinusNextPage = 129,
+        CycleCost = 2,
+        BytesUsed = 2,
+        PCStart = 0x4080,
+        PCStartHigh = 0x40E0,
+        PCStartLow = 0x4010;
+
+    MOS6502._PC = PCStart;
+    MOS6502._RAM[PCStart] = OPCODE;
+    MOS6502._RAM[PCStartHigh] = OPCODE;
+    MOS6502._RAM[PCStartLow] = OPCODE;
+    MOS6502._CYCLES = 0;
+
+    /**
+     * Test 1: Zero not set (no branch, but 2 cycles used)
+     */
+
+        // Enable overflow.
+    MOS6502._P = 0x20;
+
+    MOS6502.emulateCycle();
+
+    equal(MOS6502._PC,
+            PCStart + BytesUsed,
+        "Zero set: Program Counter set correctly.");
+
+    equal(MOS6502._CYCLES,
+        CycleCost,
+        "Zero set: Cycles set correctly.");
+
+    /**
+     * Test 2: Zero set. Branch forward to same page. (3 cycles)
+     */
+
+    MOS6502._P = 0x22;
+    MOS6502._PC = PCStart;
+    MOS6502._RAM[PCStart + 1] = relativePlusSamePage;
+    MOS6502._CYCLES = 0;
+
+    MOS6502.emulateCycle();
+
+    equal(MOS6502._PC,
+            PCStart + relativePlusSamePage,
+        "Zero set, branch forward, same page: Program Counter set correctly.");
+
+    equal(MOS6502._CYCLES,
+            CycleCost + 1,
+        "Zero set, branch forward, same page: Cycles set correctly.");
+
+    /**
+     * Test 3: Zero set. Branch backward to same page. (3 cycles)
+     */
+
+    MOS6502._P = 0x22;
+    MOS6502._PC = PCStart;
+    MOS6502._RAM[PCStart + 1] = relativeMinusSamePage;
+    MOS6502._CYCLES = 0;
+
+    MOS6502.emulateCycle();
+
+    equal(MOS6502._PC,
+            (PCStart + relativeMinusSamePage) - 256,
+        "Zero set, branch backwards, same page: Program Counter set correctly.");
+
+    equal(MOS6502._CYCLES,
+            CycleCost + 1,
+        "Zero set, branch backwards, same page: Cycles set correctly.");
+
+    /**
+     * Test 4: Zero set. Branch forward to different page. (4 cycles)
+     *
+     * NOTE: It's not possible to branch to another page from 0x4080. Using 0x40E0 for forward branch.
+     */
+
+    MOS6502._P = 0x23;
+    MOS6502._PC = PCStartHigh;
+    MOS6502._RAM[PCStartHigh + 1] = relativePlusNextPage;
+    MOS6502._CYCLES = 0;
+
+    MOS6502.emulateCycle();
+
+    equal(MOS6502._PC,
+            PCStartHigh + relativePlusNextPage,
+        "Zero set, branch forward, different page: Program Counter set correctly.");
+
+    equal(MOS6502._CYCLES,
+            CycleCost + 2,
+        "Zero set, branch forward, different page: Cycles set correctly.");
+
+    /**
+     * Test 5: Zero set. Branch backward to different page. (4 cycles)
+     */
+
+    MOS6502._P = 0x23;
+    MOS6502._PC = PCStartLow;
+    MOS6502._RAM[PCStartLow + 1] = relativeMinusNextPage;
+    MOS6502._CYCLES = 0;
+
+    MOS6502.emulateCycle();
+
+    equal(MOS6502._PC,
+            (PCStartLow + relativeMinusNextPage) - 256,
+        "Zero set, branch backwards, different page: Program Counter set correctly.");
+
+    equal(MOS6502._CYCLES,
+            CycleCost + 2,
+        "Zero set, branch backwards, different page: Cycles set correctly.");
+
+});
+
+//</editor-fold>
+
 
 /**
  * Tests to be implemented:
-
- // 0xE0 - 0xEF
- case (0xEA) : me.NOP(); break;
 
  // 0xF0 - 0xFF
  case (0xF0) : me.BEQ(); break;
